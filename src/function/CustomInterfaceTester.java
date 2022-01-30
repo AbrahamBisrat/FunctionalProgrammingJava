3352b27bfa31e01cb2ca62e98a959f7c1533bb10
Abraham Bisrat <abrahammehari0@gmail.com>
2022-01-30 16:31:31 -0600

    BiFunction for elements present only in list>

--- src/function/CustomInterfaceTester.java
+++ src/function/CustomInterfaceTester.java
@@ -3,30 +3,45 @@ package function;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class CustomInterfaceTester{	
 
 	
 	public static void main(String[] args) {
-		List<String> names = new ArrayList<>() {
+		List<String> movies = new ArrayList<>() {
 			{
 				add("wallE");
-				add("I am legend");
-				add("Back to the Future");
-				add("i Robot");
-				add("Matrix triology");
-				add("some other movies");
-				add("my hands are typing");
-				add("you could have");
-				add("a list of movies");
-				add("from the internet");
-				add("and yet here we are ");
-				add("typing movie names in");
+				add("Brave");
+				add("Matrix");
+				add("Iron-Man");
+				add("one");
+				add("two");
 			}
 		};
 		
-		System.out.println(trio.apply(names, 1, 2));
+		List<String> numbers = new ArrayList<>() {
+			{
+				add("wallE");
+				add("one");
+				add("two");
+				add("three");
+				add("four");
+				add("five");
+			}
+		};
+		
+		
+		
+		System.out.println(trio.apply(movies, 1, 2));
+		
+		System.out.println(
+				addLen.apply(movies.get(2), movies.get(movies.size() - 1)));
+		
+		System.out.println(onlyInOne.apply(movies, numbers));
+		
+		
 	}
 	
 	public static final 
@@ -38,4 +53,16 @@ public class CustomInterfaceTester{
 					   .map(ln -> ln + "\n")
 					   .sorted(Comparator.comparing(String::length))
 					   .collect(Collectors.toList());
+	
+	public static final BiFunction<String, String, Integer>
+		addLen = (x, y) -> x.length() + y.length();
+	
+	public static final BiFunction<List<String>, List<String>, List<String>>
+		onlyInOne = (firstList, secondList) 
+			-> firstList.stream()
+						.filter(x -> !secondList.contains(x))
+//						.filter(y -> !firstList.contains(y))
+						.map(x -> x + "\n")
+						.collect(Collectors.toList());
+	public static final 
 }
